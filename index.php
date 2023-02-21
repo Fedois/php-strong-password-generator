3f98c9788fee1b6b3721e8f59ff497fa5e89ab70
Fedois <fedefox14@gmail.com>
2023-02-21 14:14:19 +0100

    fine milestone 2

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-		if(isset($_GET['length'])) {
-			$length = $_GET['length'];
-
-			$password = generatePassword($length);
-		}
-
-		function generatePassword($length) {
-			$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-			$password = '';
-			
-            for($i=0; $i<$length; $i++) {
-				$password .= $chars[rand(0, strlen($chars)-1)];
-			}
-			return $password;
-		}
+	include './funcions.php'
 ?>
 
 <!DOCTYPE html>
