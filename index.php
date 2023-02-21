b2764469ac7239625c71efd29e6d7296c64545fa
Fedois <fedefox14@gmail.com>
2023-02-21 13:42:56 +0100

    inizializzazione progetto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP PSW</title>
+</head>
+<body>
+<h1>Generatore di password casuale</h1>
+	<form method="GET">
+		<label for="length">Lunghezza password:</label>
+		<input type="number" id="length" name="length" min="1" max="10" required>
+		<button type="submit">Genera password</button>
+	</form>
+</body>
+</html>
