91d0072a718a8bb93a87a1da8ce24c6810117665
Fedois <fedefox14@gmail.com>
2023-02-21 16:53:46 +0100

    pulizia codice

--- funcions.php
+++ funcions.php
@@ -1,49 +1,39 @@
 <?php
     if(isset($_GET['length'])) {
         $length = $_GET['length'];
+        $useWords = isset($_GET['words']);
+        $useNum = isset($_GET['numbers']);
+        $useSpecial = isset($_GET['special']);
 
-        $password = generatePassword($length);
+        $password = generatePassword($length, $useWords, $useNum, $useSpecial);
 
         $_SESSION['psw'] = $password;
         header("location: password.php");
     }
 
-    function generatePassword($length) {
-        if(isset($_GET['words'])){
+    function generatePassword($length, $useWords, $useNum, $useSpecial) {
+        if($useWords){
             $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            $password = '';
-            
-            for($i=0; $i<$length; $i++) {
-                $password .= $chars[rand(0, strlen($chars)-1)];
-            }
-            return $password;
         }
-        else if(isset($_GET['numbers'])){
+        else if($useNum){
             $chars = '1234567890';
-            $password = '';
-            
-            for($i=0; $i<$length; $i++) {
-                $password .= $chars[rand(0, strlen($chars)-1)];
-            }
-            return $password;
         }
-        else if(isset($_GET['special'])){
+        else if($useSpecial){
             $chars = '!@#$%^&*()_+';
-            $password = '';
-            
-            for($i=0; $i<$length; $i++) {
-                $password .= $chars[rand(0, strlen($chars)-1)];
-            }
-            return $password;
+        }
+        else if(($useWords) && ($useNum)){
+            $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         }
         else{
             $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+';
-            $password = '';
+        }
+        
+        $password = '';
             
             for($i=0; $i<$length; $i++) {
                 $password .= $chars[rand(0, strlen($chars)-1)];
             }
+        
             return $password;
-        }
     }
 ?>
