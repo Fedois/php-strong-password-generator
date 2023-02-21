9e3bcfb5dfc4e04a34d949666df2beefe2f39864
Fedois <fedefox14@gmail.com>
2023-02-21 14:13:06 +0100

    aggiunto funzione genera psw

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+		if(isset($_GET['length'])) {
+			$length = $_GET['length'];
+
+			$password = generatePassword($length);
+		}
+
+		function generatePassword($length) {
+			$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+			$password = '';
+			
+            for($i=0; $i<$length; $i++) {
+				$password .= $chars[rand(0, strlen($chars)-1)];
+			}
+			return $password;
+		}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +29,9 @@
 	<form method="GET">
 		<label for="length">Lunghezza password:</label>
 		<input type="number" id="length" name="length" min="1" max="10" required>
-		<button type="submit">Genera password</button>
+		<button type="submit">Genera</button>
 	</form>
+
+    <h2>la tua password casuale è: <?php echo $password ?></h2>
 </body>
 </html>
